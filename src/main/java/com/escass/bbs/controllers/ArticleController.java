f1382683da645483c2e572009bf853e04838fd28
escurse <av1967@naver.com>
2024-12-05 12:01:32 +0900

    bbs

--- src/main/java/com/escass/bbs/controllers/ArticleController.java
+++ src/main/java/com/escass/bbs/controllers/ArticleController.java
@@ -3,7 +3,6 @@ package com.escass.bbs.controllers;
 import com.escass.bbs.entities.ArticleEntity;
 import com.escass.bbs.results.Result;
 import com.escass.bbs.services.ArticleService;
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
@@ -11,7 +10,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
